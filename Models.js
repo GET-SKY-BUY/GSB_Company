e080a7bf6c8a8121a405b84590d6cb4b2375b446
= <ricksarkar2005@gmail.com>
2024-10-19 18:28:09 +0530

    Schema/Model for User Declared.

---
+++ Models.js
@@ -0,0 +1,112 @@
+require('dotenv').config();
+const mongoose = require("mongoose");
+const URL = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_SUBDOMAIN}.mongodb.net/${process.env.DB_NAME}`;
+mongoose.connect(URL);
+const db = mongoose.connection;
+db.on('error',(error) => {
+    console.log('MongoDB connection error:');
+});
+db.once('open',() => {
+    console.log('Connected to MongoDB database.');
+});
+const Schema = mongoose.Schema;
+const Model = mongoose.model;
+
+const UserSchema = new Schema({
+    _id:{
+        type: String,
+    },
+    Email:{
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+    },
+    PersonalData:{
+        First_Name: {
+            type: String,
+        },
+        Last_Name: {
+            type: String,
+        },
+        Mobile_Number: {
+            type: String,
+        },
+        DOB: {
+            type: Date,
+        },
+        Gender: {
+            type: String,
+            enum:["Male","Female","Other"]
+        },
+    },
+    Ban:{
+        type: String,
+        required: true,
+        default: "No",
+        trim: true,
+        enum: ['Yes', 'No'],
+    },
+    Verified:{
+        type:String,
+        required: true,
+        default: "No",
+        trim: true,
+        enum: ['Yes', 'No'],
+    },
+    createdAt: {
+        type: Date,
+    },
+    Auth:{
+        OTP:{
+            type: String,
+            trim:true,
+        },
+        OTP_Expiry:{
+            type: Date,
+        },
+        Token:{
+            type: String,
+            trim:true,
+        }
+    },
+    LoggedIn:{type:Object},
+    Cart:{type:Object},
+    BuyNow:{type:Object},
+    Order_IDs:{type:Object},
+    Address:{
+        Active_ID:{type:String},
+        List:{type:Object},
+    },
+    Search_History:{type:Object},
+    Product_History:{type:Object},
+    Interested_Search:{type:Object},
+    GSBCoins:{
+        Available:{type:Number},
+        Earned:{type:Number},
+        History:{type:Object},
+    },
+    Favourite:{type:Object},
+    Wishlist:{type:Object},
+    Notification:{type:Object},
+    Bank:{
+        Bank_Name:{type: String},
+        Beneficiary_Name:{type: String},
+        Account_Number:{type: String},
+        IFSC_Code:{type: String},
+    },
+    Refund:{type:Object},
+    Overview:{type:Object},
+    Password:{
+        type: String,
+        required: true,
+        trim:true,
+    },
+});
+
+const User = Model("User", UserSchema);
+
+module.exports = {
+    User
+}
+
