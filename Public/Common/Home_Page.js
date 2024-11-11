12ca5b756eeecb58724ad1e3ef93f00f09cb8873
= <ricksarkar2005@gmail.com>
2024-11-12 01:19:34 +0530

    Few changes

--- Public/Common/Home_Page.js
+++ Public/Common/Home_Page.js
@@ -2,3 +2,7 @@
     PRODUCT_LIST = await call(window.innerWidth, "Section1");
     console.log(PRODUCT_LIST);
 })();
+(async () => {
+    PRODUCT_LIST = await call(window.innerWidth, "Section2");
+    console.log(PRODUCT_LIST);
+})();
