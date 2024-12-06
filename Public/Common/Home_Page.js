d063d4f27ac4dfd84da19c93a41f6f53ef7ebeed
= <ricksarkar2005@gmail.com>
2024-12-06 14:27:13 +0530

    Removed

--- Public/Common/Home_Page.js
+++
@@ -1,8 +0,0 @@
-(async () => {
-    PRODUCT_LIST = await call(window.innerWidth, "Section1");
-    console.log(PRODUCT_LIST);
-})();
-(async () => {
-    PRODUCT_LIST = await call(window.innerWidth, "Section2");
-    console.log(PRODUCT_LIST);
-})();
